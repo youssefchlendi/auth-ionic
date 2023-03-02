52291014bc19f2abfea3186ff692b9153c994fd9
Mohamed youssef chlendi <youssef.chlendi@gmail.com>
2023-03-02 08:38:04 +0100

    master layout

---
+++ src/components/masterLayout.vue
@@ -0,0 +1,38 @@
+<template>
+	<ion-page>
+		<ion-header>
+			<ion-toolbar>
+				<ion-title>{{ pageTitle }}</ion-title>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content>
+			<slot> </slot>
+		</ion-content>
+	</ion-page>
+</template>
+<script>
+import {
+	IonPage,
+	IonHeader,
+	IonContent,
+	IonToolbar,
+	IonTitle
+} from "@ionic/vue";
+export default {
+	components: {
+		IonPage,
+		IonHeader,
+		IonContent,
+		IonToolbar,
+		IonTitle,
+	},
+	props: ["pageTitle"],
+
+};
+</script>
+<style scoped>
+ion-toolbar {
+	--background: var(--ion-color-primary);
+	--color: var(--ion-color-primary-contrast)
+}
+</style>
