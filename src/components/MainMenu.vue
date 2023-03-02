b8103508af807b61ffd79a00a8f99327fc0337e5
Mohamed youssef chlendi <youssef.chlendi@gmail.com>
2023-03-02 15:24:42 +0100

    craete menu component

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,27 @@
+<template>
+	<ion-menu contentId="main-content">
+		<ion-header>
+			<ion-toolbar>
+				<ion-title>Menu Content</ion-title>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content class="ion-padding">This is the menu content.</ion-content>
+	</ion-menu>
+</template>
+
+<script  lang="ts">
+import { IonMenu, IonHeader, IonToolbar, IonContent, IonTitle } from "@ionic/vue";
+export default {
+	components: {
+		IonMenu,
+		IonHeader,
+		IonToolbar,
+		IonContent,
+		IonTitle
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/masterLayout.vue
+++ src/components/masterLayout.vue
@@ -1,32 +1,75 @@
 <template>
 	<ion-page>
+	<main-menu v-if="authStore.isLoggedIn"/>
+	<div class="ion-page" id="main-content">
 		<ion-header>
 			<ion-toolbar>
+				<ion-buttons slot="start">
+					<ion-menu-button v-if="authStore.isLoggedIn"></ion-menu-button>
+				</ion-buttons>
 				<ion-title>{{ pageTitle }}</ion-title>
+				<ion-buttons slot="end">
+					<ion-button v-if="authStore.isLoggedIn" @click="logout">
+						<ion-icon slot="icon-only" :icon="logOutOutline"></ion-icon>
+					</ion-button>
+				</ion-buttons>
 			</ion-toolbar>
 		</ion-header>
 		<ion-content>
 			<slot> </slot>
 		</ion-content>
-	</ion-page>
+	</div>
+</ion-page>
 </template>
-<script>
+<script lang="ts">
 import {
-	IonPage,
 	IonHeader,
 	IonContent,
 	IonToolbar,
-	IonTitle
+	IonTitle,
+	IonButtons,
+	IonMenuButton,
+	IonPage,
+IonIcon,
+IonButton,
 } from "@ionic/vue";
+import {logOutOutline} from 'ionicons/icons';
+import MainMenu from '@/components/MainMenu.vue';
+import { useAuthStore } from '@/store/auth.store';
+import { useRouter } from 'vue-router';
 export default {
 	components: {
-		IonPage,
 		IonHeader,
 		IonContent,
 		IonToolbar,
 		IonTitle,
+		IonButtons,
+		IonButton,
+		IonMenuButton,
+		MainMenu,
+		IonIcon,
+		IonPage
 	},
-	props: ["pageTitle"],
+	props: [
+		"pageTitle"
+	],
+	setup() {
+		const authStore = useAuthStore();
+		const router = useRouter();
+		
+		const logout = () => {
+			const res = authStore.logout();
+			console.log(res);
+			if (res) {
+				router.push('/login');
+			}
+		};
+		return {
+			authStore,
+			logOutOutline,
+			logout
+		};
+	},
 
 };
 </script>
